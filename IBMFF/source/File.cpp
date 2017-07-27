fd8145e23e5297daca54396b995b3349e91fddf3
macmade <macmade@xs-labs.com>
2017-07-27 21:55:23 +0200

    Initial Xcode project.

---
+++ IBMFF/source/File.cpp
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2017 Jean-David Gadina - www.xs-labs.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+/*!
+ * @file        File.cpp
+ * @copyright   (c) 2017, Jean-David Gadina - www.xs-labs.com
+ */
+
+#include <IBMFF/File.hpp>
+
+template<>
+class XS::PIMPL::Object< IBMFF::File >::IMPL
+{
+    public:
+        
+        IMPL( void );
+        IMPL( const std::string & path );
+        IMPL( const IMPL & o );
+        ~IMPL( void );
+        
+        std::string _path;
+};
+
+#define XS_PIMPL_CLASS IBMFF::File
+#include <XS/PIMPL/Object-IMPL.hpp>
+
+namespace IBMFF
+{
+    File::File( const std::string & path )
+    {
+        ( void )path;
+    }
+}
+
+XS::PIMPL::Object< IBMFF::File >::IMPL::IMPL( void )
+{}
+
+XS::PIMPL::Object< IBMFF::File >::IMPL::IMPL( const std::string & path ):
+    _path( path )
+{}
+
+XS::PIMPL::Object< IBMFF::File >::IMPL::IMPL( const IMPL & o ):
+    _path( o._path )
+{}
+
+XS::PIMPL::Object< IBMFF::File >::IMPL::~IMPL( void )
+{}
