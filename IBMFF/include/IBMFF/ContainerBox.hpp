81f82bae1b9ab277cda27f0da48a3d9425f5bbf4
macmade <macmade@xs-labs.com>
2017-09-05 11:33:25 +0200

    ContainerBox class.

---
+++ IBMFF/include/IBMFF/ContainerBox.hpp
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2017 Jean-David Gadina - www.xs-labs.com / www.imazing.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+/*!
+ * @header      ContainerBox.hpp
+ * @copyright   (c) 2017, Jean-David Gadina - www.xs-labs.com / www.imazing.com
+ */
+
+#ifndef IBMFF_CONTAINER_BOX_HPP
+#define IBMFF_CONTAINER_BOX_HPP
+
+#include <XS/PIMPL/Object.hpp>
+#include <IBMFF/Box.hpp>
+#include <vector>
+#include <memory>
+#include <ostream>
+
+namespace IBMFF
+{
+    class ContainerBox: public Box, public XS::PIMPL::Object< ContainerBox >
+    {
+        public:
+            
+            using XS::PIMPL::Object< ContainerBox >::impl;
+            using Box::Box;
+            
+            void ReadData( const Parser & parser, BinaryStream & stream ) override;
+            
+            void AddBox( std::shared_ptr< Box > box );
+            
+            std::vector< std::shared_ptr< Box > > GetBoxes( void ) const;
+            
+            void WriteDescription( std::ostream & os, std::size_t indentLevel ) const override;
+    };
+}
+
+#endif /* IBMFF_CONTAINER_BOX_HPP */
