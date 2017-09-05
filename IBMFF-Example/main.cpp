3471d91f6bcf161b143376234580a643bb37ec24
macmade <macmade@xs-labs.com>
2017-09-05 21:26:51 +0200

    Example in progress...

--- IBMFF-Example/main.cpp
+++ IBMFF-Example/main.cpp
@@ -31,6 +31,11 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <iomanip>
+
+void PrintData( const IBMFF::Box & mdat, IBMFF::ILOC::Item::Extent & extent );
+void PrintDataLine( const std::vector< uint8_t > & data, uint64_t length );
 
 int main( int argc, const char * argv[] )
 {
@@ -66,8 +71,11 @@ int main( int argc, const char * argv[] )
         {
             std::shared_ptr< IBMFF::FTYP > ftyp;
             std::shared_ptr< IBMFF::META > meta;
-            std::shared_ptr< IBMFF::ILOC > iloc;
             std::shared_ptr< IBMFF::Box  > mdat;
+            std::shared_ptr< IBMFF::IINF > iinf;
+            std::shared_ptr< IBMFF::ILOC > iloc;
+            IBMFF::ILOC::Item::Extent      exif;
+            IBMFF::ILOC::Item::Extent      hvc1;
             
             ftyp = file->GetTypedBox< IBMFF::FTYP >( "ftyp" );
             meta = file->GetTypedBox< IBMFF::META >( "meta" );
@@ -75,23 +83,171 @@ int main( int argc, const char * argv[] )
             
             if
             (
-                   ftyp == nullptr
-                || meta == nullptr
-                || mdat == nullptr
+                   ftyp                  == nullptr
+                || meta                  == nullptr
+                || mdat                  == nullptr
                 || ftyp->GetMajorBrand() != "heic"
             )
             {
                 continue;
             }
             
+            iinf = meta->GetTypedBox< IBMFF::IINF >( "iinf" );
             iloc = meta->GetTypedBox< IBMFF::ILOC >( "iloc" );
                 
-            if( iloc == nullptr )
+            if( iloc == nullptr || iinf == nullptr )
             {
                 continue;
+            }
+            
+            for( const auto & entry: iinf->GetEntries()  )
+            {
+                if( entry->GetItemType() == "Exif" )
+                {
+                    for( const auto & item: iloc->GetItems() )
+                    {
+                        if
+                        (
+                               item.GetItemID()         == entry->GetItemID()
+                            && item.GetExtents().size() != 0
+                        )
+                        {
+                            exif = item.GetExtents()[ 0 ];
+                        }
+                    }
+                }
+                else if( entry->GetItemType() == "hvc1" )
+                {
+                    for( const auto & item: iloc->GetItems() )
+                    {
+                        if
+                        (
+                               item.GetItemID()                   == entry->GetItemID()
+                            && item.GetExtents().size()           != 0
+                            && item.GetExtents()[ 0 ].GetLength() >  hvc1.GetLength()
+                        )
+                        {
+                            hvc1 = item.GetExtents()[ 0 ];
+                        }
+                    }
+                }
+            }
+            
+            if( exif.GetLength() > 0 )
+            {
+                std::cout << "Found EXIF data ( index = " << exif.GetIndex() << ", offset = " << exif.GetOffset() << ", length = " << exif.GetLength() << " ):" << std::endl;
+                std::cout << std::endl;
+                
+                PrintData( *( mdat ), exif );
+                
+                std::cout << std::endl;
+            }
+            
+            if( hvc1.GetLength() > 0 )
+            {
+                std::cout << "Found HVC1 data ( index = " << hvc1.GetIndex() << ", offset = " << hvc1.GetOffset() << ", length = " << hvc1.GetLength() << " ):" << std::endl;
+                std::cout << std::endl;
+                
+                PrintData( *( mdat ), hvc1 );
+                
+                std::cout << std::endl;
             }
         }
     }
     
     return EXIT_SUCCESS;
+}
+
+template< typename _T_ >
+std::vector< _T_ > Slice( const std::vector< _T_ > & v, uint64_t start, uint64_t length )
+{
+    std::vector< uint8_t >::difference_type s;
+    std::vector< uint8_t >::difference_type l;
+    
+    s = static_cast< decltype( s ) >( start );
+    l = static_cast< decltype( l ) >( length );
+    
+    if( v.begin() + s > v.end() )
+    {
+        return {};
+    }
+    
+    if( v.begin() + s + l > v.end() )
+    {
+        return std::vector< uint8_t >( v.begin() + s, v.end() );
+    }
+    
+    return std::vector< uint8_t >( v.begin() + s, v.begin() + s + l );
+}
+
+void PrintData( const IBMFF::Box & mdat, IBMFF::ILOC::Item::Extent & extent )
+{
+    std::vector< uint8_t > data;
+    std::vector< uint8_t > part;
+    uint64_t               i;
+    
+    data = Slice( mdat.GetData(), extent.GetOffset(), extent.GetLength() );
+    
+    if( data.size() )
+    {
+        i = 0;
+        
+        while( 1 )
+        {
+            part = Slice( data, i, 30 );
+            
+            if( part.size() == 0 )
+            {
+                break;
+            }
+            
+            PrintDataLine( part, 30 );
+            
+            i += 20;
+        }
+    }
+}
+
+void PrintDataLine( const std::vector< uint8_t > & data, uint64_t length )
+{
+    uint64_t i;
+    
+    for( auto byte: data )
+    {
+        {
+            std::stringstream ss;
+            
+            ss << std::hex
+               << std::uppercase
+               << std::setfill( '0' )
+               << std::setw( 2 )
+               << static_cast< uint32_t >( byte );
+            
+            std::cout << ss.str() << " ";
+        }
+    }
+    
+    if( data.size() < length )
+    {
+        for( i = 0; i < length - data.size(); i++ )
+        {
+            std::cout << "   ";
+        }
+    }
+    
+    std::cout << "| ";
+    
+    for( auto byte: data )
+    {
+        if( isprint( byte ) && !isspace( byte ) )
+        {
+            std::cout << byte;
+        }
+        else
+        {
+            std::cout << ".";
+        }
+    }
+    
+    std::cout << std::endl;
 }
