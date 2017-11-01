b91314affa6d5c3f64c3e5dc90bae9748e9983b6
macmade <macmade@xs-labs.com>
2017-11-01 13:51:55 +0100

    Inline documentation in progress...

--- ISOBMFF/include/ISOBMFF/Matrix.hpp
+++ ISOBMFF/include/ISOBMFF/Matrix.hpp
@@ -38,39 +38,187 @@
 
 namespace ISOBMFF
 {
+    /*!
+     * @class       Matrix
+     * @abstract    Represents a transformation of video images for presentation.
+     */
     class ISOBMFF_EXPORT Matrix: public XS::PIMPL::Object< Matrix >, public DisplayableObject
     {
         public:
             
             using XS::PIMPL::Object< Matrix >::impl;
             
+            /*!
+             * @function    Matrix
+             * @abstract    Default constructor.
+             */
             Matrix( void );
+            
+            /*!
+             * @function    Matrix
+             * @abstract    Creates a Matrix object.
+             * @param       a   The matrix A coordinate.
+             * @param       b   The matrix B coordinate.
+             * @param       u   The matrix U coordinate.
+             * @param       c   The matrix C coordinate.
+             * @param       d   The matrix D coordinate.
+             * @param       v   The matrix V coordinate.
+             * @param       x   The matrix X coordinate.
+             * @param       y   The matrix Y coordinate.
+             * @param       w   The matrix W coordinate.
+             */
             Matrix( uint32_t a, uint32_t b, uint32_t u, uint32_t c, uint32_t d, uint32_t v, uint32_t x, uint32_t y, uint32_t w );
             
+            /*!
+             * @function    GetName
+             * @abstract    Gets the object's name.
+             * @result      The object's name
+             * @see         DisplayableObject
+             */
             std::string GetName( void ) const override;
             
+            /*!
+             * @function    GetA
+             * @abstract    Gets the matrix A coordinate.
+             * @result      The matrix A coordinate.
+             */
             uint32_t GetA( void ) const;
+            
+            /*!
+             * @function    GetB
+             * @abstract    Gets the matrix B coordinate.
+             * @result      The matrix B coordinate.
+             */
             uint32_t GetB( void ) const;
+            
+            /*!
+             * @function    GetU
+             * @abstract    Gets the matrix U coordinate.
+             * @result      The matrix U coordinate.
+             */
             uint32_t GetU( void ) const;
+            
+            /*!
+             * @function    GetC
+             * @abstract    Gets the matrix C coordinate.
+             * @result      The matrix C coordinate.
+             */
             uint32_t GetC( void ) const;
+            
+            /*!
+             * @function    GetD
+             * @abstract    Gets the matrix D coordinate.
+             * @result      The matrix D coordinate.
+             */
             uint32_t GetD( void ) const;
+            
+            /*!
+             * @function    GetV
+             * @abstract    Gets the matrix V coordinate.
+             * @result      The matrix V coordinate.
+             */
             uint32_t GetV( void ) const;
+            
+            /*!
+             * @function    GetX
+             * @abstract    Gets the matrix X coordinate.
+             * @result      The matrix X coordinate.
+             */
             uint32_t GetX( void ) const;
+            
+            /*!
+             * @function    GetY
+             * @abstract    Gets the matrix Y coordinate.
+             * @result      The matrix Y coordinate.
+             */
             uint32_t GetY( void ) const;
+            
+            /*!
+             * @function    GetW
+             * @abstract    Gets the matrix W coordinate.
+             * @result      The matrix W coordinate.
+             */
             uint32_t GetW( void ) const;
             
+            /*!
+             * @function    SetA
+             * @abstract    Sets the matrix A coordinate.
+             * @param       value   The matrix A coordinate to set.
+             */
             void SetA( uint32_t value );
+            
+            /*!
+             * @function    SetWB
+             * @abstract    Sets the matrix B coordinate.
+             * @param       value   The matrix B coordinate to set.
+             */
             void SetB( uint32_t value );
+            
+            /*!
+             * @function    SetU
+             * @abstract    Sets the matrix U coordinate.
+             * @param       value   The matrix U coordinate to set.
+             */
             void SetU( uint32_t value );
+            
+            /*!
+             * @function    SetC
+             * @abstract    Sets the matrix C coordinate.
+             * @param       value   The matrix C coordinate to set.
+             */
             void SetC( uint32_t value );
+            
+            /*!
+             * @function    SetD
+             * @abstract    Sets the matrix D coordinate.
+             * @param       value   The matrix D coordinate to set.
+             */
             void SetD( uint32_t value );
+            
+            /*!
+             * @function    SetV
+             * @abstract    Sets the matrix V coordinate.
+             * @param       value   The matrix V coordinate to set.
+             */
             void SetV( uint32_t value );
+            
+            /*!
+             * @function    SetX
+             * @abstract    Sets the matrix X coordinate.
+             * @param       value   The matrix X coordinate to set.
+             */
             void SetX( uint32_t value );
+            
+            /*!
+             * @function    SetY
+             * @abstract    Sets the matrix Y coordinate.
+             * @param       value   The matrix Y coordinate to set.
+             */
             void SetY( uint32_t value );
+            
+            /*!
+             * @function    SetW
+             * @abstract    Sets the matrix W coordinate.
+             * @param       value   The matrix W coordinate to set.
+             */
             void SetW( uint32_t value );
             
-            void                                                         WriteDescription( std::ostream & os, std::size_t indentLevel ) const override;
-            virtual std::vector< std::pair< std::string, std::string > > GetDisplayableProperties( void )                               const override;
+            /*!
+             * @function    WriteDescription
+             * @abstract    Writes the matrix description to an output stream.
+             * @param       os              The output stream in which to write the description.
+             * @param       indentLevel     The requested indent level.
+             * @see         DisplayableObject
+             */
+            void WriteDescription( std::ostream & os, std::size_t indentLevel ) const override;
+            
+            /*!
+             * @function    GetDisplayableProperties
+             * @abstract    Gets the matrix displayable properties.
+             * @result      The matrix displayable properties.
+             * @see         DisplayableObject
+             */
+            virtual std::vector< std::pair< std::string, std::string > > GetDisplayableProperties( void ) const override;
     };
 }
 
--- ISOBMFF/include/ISOBMFF/Parser.hpp
+++ ISOBMFF/include/ISOBMFF/Parser.hpp
@@ -42,46 +42,172 @@
 
 namespace ISOBMFF
 {
+    /*!
+     * @class       Parser
+     * @abstract    ISO media file parser.
+     */
     class ISOBMFF_EXPORT Parser: public XS::PIMPL::Object< Parser >
     {
         public:
             
             using XS::PIMPL::Object< Parser >::impl;
             
+            /*!
+             * @enum        StringType
+             * @abstract    Possible types for strings in an ISO media file.
+             * @discussion  ISO/IEC 14496-12 only specifies UTF-8 NULL
+             *              terminated strings, but QuickTime actually uses
+             *              Pascal strings.
+             * @constant    NULLTerminated  UTF-8 NULL terminated strings.
+             * @constant    Pascal          Pascal strings (length prefixed).
+             */
             enum class StringType: int
             {
                 NULLTerminated,
                 Pascal
             };
             
+            /*!
+             * @enum        Options
+             * @abstract    Parser options.
+             * @constant    SkipMDATData    Do not keep data found in MDAT boxes.
+             */
             enum class Options: uint64_t
             {
                 SkipMDATData = 1 << 0
             };
             
+            /*!
+             * @function    Parser
+             * @abstract    Default constructor.
+             */
             Parser( void );
+            
+            /*!
+             * @function    Parser
+             * @abstract    Creates a parser for a file.
+             * @param       path    The file's path.
+             */
             Parser( const std::string & path );
             
+            /*!
+             * @function    RegisterBox
+             * @abstract    Registers a custom box type.
+             * @param       type        The custom box type (four character string).
+             * @param       createBox   A lambda returning a new box of the custom type.
+             * @discussion  When encountering a box of the specified custom type,
+             *              the parser will invoke the lambda to create a new
+             *              object of the correct type.
+             */
             void RegisterBox( const std::string & type, const std::function< std::shared_ptr< Box >( void ) > & createBox );
+            
+            /*!
+             * @function    RegisterContainerBox
+             * @abstract    Registers a custom box type as a container box.
+             * @param       type    The custom box type (four character string).
+             */
             void RegisterContainerBox( const std::string & type );
             
+            /*!
+             * @function    CreateBox
+             * @abstract    Creates a new box for a specific type.
+             * @param       type    The box type (four character string).
+             * @result      A new box.
+             */
             std::shared_ptr< Box > CreateBox( const std::string & type ) const;
             
+            /*!
+             * @function    Parse
+             * @abstract    Parses a file.
+             * @discussion  This will discard any previously parsed file.
+             * @param       path    The file's path.
+             */
             void Parse( const std::string & path ) ISOBMFF_NOEXCEPT( false );
             
+            /*!
+             * @function    GetFile
+             * @abstract    Upon successfull parsing, gets the file object.
+             * @result      The file object, or nullptr.
+             */
             std::shared_ptr< File > GetFile( void ) const;
             
+            /*!
+             * @function    GetPreferredStringType
+             * @abstract    Gets the preferred string type used in the parser.
+             * @result      The preferred string type.
+             * @see         StringType
+             */
             StringType GetPreferredStringType( void ) const;
-            void       SetPreferredStringType( StringType value );
             
+            /*!
+             * @function    SetPreferredStringType
+             * @abstract    Sets the preferred string type to use in the parser.
+             * @param       value   The preferred string type.
+             * @see         StringType
+             */
+            void SetPreferredStringType( StringType value );
+            
+            /*!
+             * @function    GetOptions
+             * @abstract    Gets the parser options.
+             * @result      The parser options.
+             * @see         Options
+             */
             uint64_t GetOptions( void ) const;
-            void     SetOptions( uint64_t value );
-            void     AddOption( Options option );
-            void     RemoveOption( Options option );
-            bool     HasOption( Options option );
             
+            /*!
+             * @function    SetOptions
+             * @abstract    Sets the parser options.
+             * @param       value   The parser options.
+             * @see         Options
+             */
+            void SetOptions( uint64_t value );
+            
+            /*!
+             * @function    AddOption
+             * @abstract    Sets a specific parser option.
+             * @param       option  The option to set/add.
+             * @see         Options
+             */
+            void AddOption( Options option );
+            
+            /*!
+             * @function    RemoveOption
+             * @abstract    Removes a specific parser option.
+             * @param       option  The option to remove.
+             * @see         Options
+             */
+            void RemoveOption( Options option );
+            
+            /*!
+             * @function    HasOption
+             * @abstract    Checks if a specific parser option is set.
+             * @param       option  The option to check for.
+             * @result      true if the option is set, otherwise false.
+             * @see         Options
+             */
+            bool HasOption( Options option );
+            
+            /*!
+             * @function    GetInfo
+             * @abstract    Gets an info value in the parser.
+             * @param       key The info key.
+             * @result      The value for the info key, or nullptr.
+             * @see         SetInfo
+             */
             const void * GetInfo( const std::string & key );
-            void         SetInfo( const std::string & key, void * value );
+            
+            /*!
+             * @function    SetInfo
+             * @abstract    Sets an info value in the parser.
+             * @discussion  This method can be used to store any kind of
+             *              contextual information that may be useful while
+             *              parsing.
+             * @param       key     The info key.
+             * @param       value   The info value.
+             * @see         GetInfo
+             */
+            void SetInfo( const std::string & key, void * value );
     };
 }
 
