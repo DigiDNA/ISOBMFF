8df12c3f3a3b0cd99b93d27d526e65191ed52326
Chema Gonzalez <chemag@meta.com>
2024-07-02 20:53:00 -0700

    add STTS box

--- ISOBMFF/source/Parser.cpp
+++ ISOBMFF/source/Parser.cpp
@@ -57,6 +57,7 @@
 #include <ISOBMFF/PIXI.hpp>
 #include <ISOBMFF/IPCO.hpp>
 #include <ISOBMFF/STSD.hpp>
+#include <ISOBMFF/STTS.hpp>
 #include <ISOBMFF/FRMA.hpp>
 #include <ISOBMFF/SCHM.hpp>
 #include <map>
@@ -367,6 +368,7 @@ namespace ISOBMFF
         this->RegisterBox( "pixi", [ = ]() -> std::shared_ptr< Box > { return std::make_shared< PIXI >(); } );
         this->RegisterBox( "ipco", [ = ]() -> std::shared_ptr< Box > { return std::make_shared< IPCO >(); } );
         this->RegisterBox( "stsd", [ = ]() -> std::shared_ptr< Box > { return std::make_shared< STSD >(); } );
+        this->RegisterBox( "stts", [ = ]() -> std::shared_ptr< Box > { return std::make_shared< STTS >(); } );
         this->RegisterBox( "frma", [ = ]() -> std::shared_ptr< Box > { return std::make_shared< FRMA >(); } );
         this->RegisterBox( "schm", [ = ]() -> std::shared_ptr< Box > { return std::make_shared< SCHM >(); } );
     }
