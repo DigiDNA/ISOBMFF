4acb5d03b4c2a5d65e79f50822752250fa876a45
Chema Gonzalez <chemag@meta.com>
2025-06-24 02:54:39 -0700

    isobmff: test new build system

--- fuzz/Parser_fuzzer.cpp
+++ fuzz/Parser_fuzzer.cpp
@@ -9,16 +9,17 @@
 #include <ISOBMFF.hpp>        // for various
 #include <Parser.hpp> // for Parser
 
+
 // libfuzzer infra to test the fuzz target
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   const std::vector<uint8_t> buffer_vector = {data, data + size};
   {
-    ISOBMFF::Parser parser;
-    try {
-      parser.Parse(buffer_vector);
-    } catch (std::exception &e) {
-      fprintf(stderr, "Caught exception: %s\n", e.what());
-    }
+  ISOBMFF::Parser parser;
+  try {
+    parser.Parse(buffer_vector);
+  } catch (std::exception &e) {
+    fprintf(stderr, "Caught exception: %s\n", e.what());
+  }
   }
   return 0;
 }
