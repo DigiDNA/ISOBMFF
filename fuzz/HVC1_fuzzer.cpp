8ce92fa8119fb31237468731506b5ca2f3878910
Marko Panayotov <panayotovmarko@meta.com>
2025-07-14 13:31:58 +0300

    isobmff: add fuzzing to HVC1 atom parser

---
+++ fuzz/HVC1_fuzzer.cpp
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (c) Meta Platforms, Inc. and its affiliates.
+ */
+
+// This file was auto-generated using fuzz/converter.py from
+// HVC1_unittest.cpp.
+// Do not edit directly.
+
+#include <ISOBMFF.hpp>                  // for various
+#include <BinaryDataStream.hpp> // for BinaryDataStream
+#include <Parser.hpp>           // for Parser
+
+
+// libfuzzer infra to test the fuzz target
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  const std::vector<uint8_t> buffer_vector = {data, data + size};
+  {
+  ISOBMFF::BinaryDataStream stream(buffer_vector);
+  ISOBMFF::Parser parser;
+  std::shared_ptr<ISOBMFF::Box> box = parser.CreateBox("hvc1");
+  try {
+    if (box != nullptr) {
+      box->ReadData(parser, stream);
+    }
+  } catch (std::exception &e) {
+    fprintf(stderr, "Caught exception: %s\n", e.what());
+  }
+  }
+  return 0;
+}
